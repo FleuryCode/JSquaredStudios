511e7d97269cec090bce2476ed009641af320761
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-01-16 23:26:16 +0100

    Adding the navbar component. Still needing to make it responsive to size and adding a burger menu when getting into tablet / mobile territory.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import LogoImage from '../components/LogoImage';
 import FadeIn from 'react-fade-in';
+import Navbar from '../components/Navbar';
 
 
 class App extends React.Component {
   render() {
     return (
       <div className='tc pa3 App'>
+        <Navbar/>
         <h1 className='f1'>We are Currently Building our Website</h1>
         <h2 className='f2'>Feel Free to Contact Us</h2>
         <FadeIn transitionDuration={3000}>
