81ba53fd1f6e300ab92d8afb9aabd97c53b798c4
Fleury Code <julienfleury93@gmail.com>
2022-01-30 19:48:30 +0100

    Changing functions to work hopefully on mac touch wheel.

--- src/components/HeaderImage/HeaderImage.jsx
+++ src/components/HeaderImage/HeaderImage.jsx
@@ -4,37 +4,58 @@ import TestImage from './bopCelis.jpg';
 import TestImage2 from './colorfulBrunch.jpg';
 import TestImage3 from './comSandwich.jpg';
 
-let scrollValue = 1;
-window.addEventListener('mousewheel', function (e) {
-    if (e.deltaY / 120 > 0) {
-        if (scrollValue === 3) {
-            scrollValue = 1;
-        } else {
-            scrollValue++;
-        }
-    } else {
-        if (scrollValue === 1) {
-            scrollValue = 3;
-        } else {
-            scrollValue--;
-        }
-    }
-    const imageOne = 'heroImage-1';
-    const imageTwo = 'heroImage-2';
-    const imageThree = 'heroImage-3';
 
-    if(scrollValue === 1) {
-        this.document.getElementById(imageTwo).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageThree).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageOne).classList.add('mainImageDisplay');
-    }else if(scrollValue === 2) {
-        this.document.getElementById(imageOne).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageThree).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageTwo).classList.add('mainImageDisplay');
-    }else if(scrollValue === 3) {
-        this.document.getElementById(imageOne).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageTwo).classList.remove('mainImageDisplay');
-        this.document.getElementById(imageThree).classList.add('mainImageDisplay');
+
+let index = 1;
+let endAnim = true;
+
+const mouseWheelUp = () => {
+    endAnim = false;
+    document.getElementById('heroImage-' + (index)).classList.add('mainImageDisplay');
+    document.getElementById('heroImage-' + (index - 1)).classList.remove('mainImageDisplay');
+    setTimeout(() => {
+        endAnim = true;
+    }, 800);
+}
+
+const mouseWheelDown = () => {
+    endAnim = false;
+    document.getElementById('heroImage-' + (index)).classList.add('mainImageDisplay');
+    document.getElementById('heroImage-' + (index + 1)).classList.remove('mainImageDisplay');
+    setTimeout(() => {
+        endAnim = true;
+    }, 800);
+}
+
+document.body.addEventListener('wheel', function (e) {
+    if (endAnim) {
+        if (e.deltaY / 120 > 0) {
+            if (index === 1) {
+                index = 3;
+                endAnim = false;
+                document.getElementById('heroImage-' + (index)).classList.add('mainImageDisplay');
+                document.getElementById('heroImage-' + (index - 2)).classList.remove('mainImageDisplay');
+                setTimeout(() => {
+                    endAnim = true;
+                }, 800);
+            } else {
+                index--;
+                mouseWheelDown();
+            }
+        } else {
+            if (index === 3) {
+                index = 1;
+                endAnim = false;
+                document.getElementById('heroImage-' + (index)).classList.add('mainImageDisplay');
+                document.getElementById('heroImage-' + (index + 2)).classList.remove('mainImageDisplay'); //index + 2 for when index === 3
+                setTimeout(() => {
+                    endAnim = true;
+                }, 800);
+            } else {
+                index++;
+                mouseWheelUp();
+            }
+        }
     }
 });
 
@@ -42,6 +63,7 @@ const HeaderImage = () => {
     return (
         <div className='container-fluid'>
             <div className="row">
+                <button>Test</button>
                 <div className="col-12 imageContainer">
                     <img id='heroImage-1' className='mainImage mainImageDisplay' src={TestImage} alt="Testing" />
                     <img id='heroImage-2' className='mainImage' src={TestImage2} alt="Testing" />
